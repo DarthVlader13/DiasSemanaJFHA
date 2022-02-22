b1f2704d425f530fb93d39b0f733ebd0d4727bc5
josey <josey@Jose>
2022-02-22 20:13:18 +0100

    JFHA: con los test de prueba

---
+++ src/diasSemana/DiasSemanaTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package diasSemana;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author josey
+ *
+ */
+class DiasSemanaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	public static void String DiasSemanaTest(int numeroDia) {
+		String diaSemana;
+	}
+}
